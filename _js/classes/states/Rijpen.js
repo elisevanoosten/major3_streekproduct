20389046bb8c3d36413c01132fe51cb8d508f871
elisevanoosten <elise.van.oosten@student.howest.be>
2016-01-27 02:23:13 +0100

    recepten en verkooppunten af

--- _js/classes/states/Rijpen.js
+++ _js/classes/states/Rijpen.js
@@ -1,17 +1,19 @@
-var scaling = 1;
+var scaling;
 var bounds;
 var smaller
 
 export default class Rijpen extends Phaser.State {
   create() {
-    this.bg = this.game.add.sprite(100,70, '4_bg')
-    this.kaasrek = this.game.add.sprite(100,450, '4_kaasrek')
+    this.bg = this.game.add.sprite(100,70, '4_bg');
+    this.kaasrek = this.game.add.sprite(100,450, '4_kaasrek');
+
     // nav
     this.nextButton = this.game.add.button(this.game.width -30, 600, 'arrow', this.nextClick, this);
     this.nextButton.anchor.setTo(0.5, 0.5);
     this.previousButton = this.game.add.button(30, 600, 'arrow', this.previousClick, this);
     this.previousButton.anchor.setTo(0.5, 0.5);
     this.previousButton.rotation = 3.1;
+
     // statusbar
     this.timeline = this.game.add.sprite(this.game.width/2, 600, '4_status')
     this.timeline.anchor.setTo(0.5, 0.5);
@@ -22,6 +24,7 @@ export default class Rijpen extends Phaser.State {
     this.kaasrek.anchor.setTo(0.5, 0.5);
 
     // bounds
+    scaling = 1;
     bounds = new Phaser.Rectangle(0, 240, this.game.width, 600 - 240);
     var graphics = this.game.add.graphics(bounds.x, bounds.y);
     graphics.drawRect(0,0,bounds.width,bounds.height);
